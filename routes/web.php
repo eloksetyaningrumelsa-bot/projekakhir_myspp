bcb2d9e13afc5679655fc2b286acf7dbe29031c4
nana <cangtipbaby@gmail.com>
2025-08-29 09:53:20 +0700

    benahi route

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('Dashboard');
 });
-Route::get('datasiswa', function () {
-    return view('datasiswa');
+Route::get('Datasiswa', function () {
+    return view('Datasiswa');
 });
