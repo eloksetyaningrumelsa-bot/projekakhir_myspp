a73adbee49456bdcaaddd950cf13773e58e47e8d
nana <cangtipbaby@gmail.com>
2025-08-29 09:24:58 +0700

    isi data siswa

--- resources/views/data_siswa.blade.php
+++ resources/views/data_siswa.blade.php
@@ -1,12 +1,72 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>coba</title>
-</head>
-<body>
-    <h1>jsjsl</h1>
-</body>
-</html>
+@extends ('master')
+@section('isi')
+<div id="layoutSidenav_content">
+            <main>
+                <div class="container-fluid px-4 mt-3">                  
+                    <!-- TABEL -->
+                    <div class="card mb-4">
+                        <div class="card-header">
+                            <i class="fas fa-table me-1"></i>
+                            Data Tagihan Terbaru
+                        </div>
+                        <div class="card-body">
+                            <table id="datatablesSimple" class="table table-bordered">
+                                <thead>
+                                    <tr>
+                                        <th>No</th>
+                                        <th>Nama</th>
+                                        <th>Alamat</th>
+                                        <th>Kelas</th>
+                                        <th>Status</th>
+                                        <th>Aksi</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <tr>
+                                        <td>1</td>
+                                        <td>Elsa Elok Setyaningrum</td>
+                                        <td>Tokyo</td>
+                                        <td>XI RPL 1</td>
+                                        <td>
+                                            <button class="btn btn-sm btn-success status-btn">Lunas</button>
+                                        </td>
+                                        <td>$170,750</td>
+                                    </tr>
+                                    <tr>
+                                        <td>2</td>
+                                        <td>Della Sabrina Indah P</td>
+                                        <td>Tokyo</td>
+                                        <td>X AKL</td>
+                                        <td>
+                                            <button class="btn btn-sm btn-danger status-btn">Belum Bayar</button>
+                                        </td>
+                                        <td>$170,750</td>
+                                    </tr>
+                                    <tr>
+                                        <td>3</td>
+                                        <td>Garrett Winters</td>
+                                        <td>Tokyo</td>
+                                        <td>X AKL</td>
+                                        <td>
+                                            <button class="btn btn-sm btn-success status-btn">Lunas</button>
+                                        </td>
+                                        <td>$170,750</td>
+                                    </tr>
+                                    <!-- Tambah data lainnya -->
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+                </div>
+            </main>
+
+            <!-- FOOTER -->
+            <footer class="py-4 bg-light mt-auto">
+                <div class="container-fluid px-4">
+                    <div class="d-flex align-items-center justify-content-between small">
+                        <div class="text-muted">Copyright &copy; MySPP BRILLIANT 2025 RPL SMK PGRI WLINGI</div>
+                    </div>
+                </div>
+            </footer>
+        </div>
+@endsection
